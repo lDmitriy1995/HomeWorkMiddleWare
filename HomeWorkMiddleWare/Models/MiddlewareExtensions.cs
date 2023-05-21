5c21bae22e9add7b75145dba085cab4706808074
Admin <Admin@DESKTOP-QQR48TL>
2023-05-21 18:59:06 +0600

    Добавьте файлы проекта.

---
+++ HomeWorkMiddleWare/Models/MiddlewareExtensions.cs
@@ -0,0 +1,28 @@
+﻿using HomeWorkMiddleWare.Models;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+public static class MiddlewareExtensions
+{
+    public static IApplicationBuilder UseRequestHeadersMiddleware(this IApplicationBuilder app)
+    {
+        return app.UseMiddleware<RequestHeadersMiddleware>();
+    }
+}
+
+public class Startup
+{
+    // ...
+
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ILoggerFactory loggerFactory)
+    {
+        // ...
+
+        app.UseRequestHeadersMiddleware();
+
+        // ...
+    }
+}
---
+++ HomeWorkMiddleWare/Models/RequestHeadersMiddleware.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using System.Threading.Tasks;
+
+namespace HomeWorkMiddleWare.Models
+
+{
+    public class RequestHeadersMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly ILogger<RequestHeadersMiddleware> _logger;
+        private static int _requestCount = 0;
+
+        public RequestHeadersMiddleware(RequestDelegate next, ILogger<RequestHeadersMiddleware> logger)
+        {
+            _next = next;
+            _logger = logger;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            _requestCount++;
+            _logger.LogInformation($"Request #{_requestCount}:");
+            foreach (var header in context.Request.Headers)
+            {
+                _logger.LogInformation($"{header.Key}: {header.Value}");
+            }
+
+            await _next(context);
+        } 
+    }
+}
